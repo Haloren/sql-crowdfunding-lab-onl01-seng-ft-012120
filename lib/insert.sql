2498a499dd233c36b9f789db53a6f91b5fe8830b
Kyle McKenna <mckennakyler@gmail.com>
2020-02-19 04:21:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,58 @@
-INSERT INTO
+INSERT INTO pledges (amount, user_id, project_id) VALUES 
+(), 
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+()
+;
 
+INSERT INTO users (name, age) VALUES 
+(), 
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+()
+;
 
 INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-(), (),
+(), 
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+()
+;
