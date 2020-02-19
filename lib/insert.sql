8feac08f164aa6c4e06f717e5660509f2d5b7a9b
Kyle McKenna <mckennakyler@gmail.com>
2020-02-19 04:24:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -55,14 +55,5 @@ INSERT INTO users (name, age) VALUES
 ;
 
 INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-(), 
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-()
+["Animal shelter needs dog food", 210], ["Help me buy a guitar", 98], ["Help save birds of paradise", 170], ["I have bed bugs!", 740], ["I want to teach English in China", 200], ["Iguana needs tail operation", 1035.5], ["My book on SQL", 20], ["The next Harry Potter", 120], ["The next Inna-Gadda-Davida", 342], ["Voldement needs a body", 489]
 ;
