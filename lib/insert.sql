b7f95d5157588ebfc5ff5767e90397c720b74b3a
Kyle McKenna <mckennakyler@gmail.com>
2020-02-19 04:17:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,4 @@
-INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
-INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO
+
+
 INSERT INTO projects ( title, category, funding_goal, start_date, end_date) VALUES
