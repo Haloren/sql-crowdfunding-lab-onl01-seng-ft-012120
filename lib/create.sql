aa536853494fd5466a5922c45e1fe6c72082d7bd
Kyle McKenna <mckennakyler@gmail.com>
2020-02-19 04:07:56 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -8,13 +8,14 @@ end_date DATE
 );
 
 CREATE TABLE users (
-
-
-
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
 );
 
 CREATE TABLE pledges (
-
-
-
+id INTEGER PRIMARY KEY,
+amount INTEGER,
+user_id INTEGER,
+project_id INTEGER
 );
